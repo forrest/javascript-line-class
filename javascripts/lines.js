9d90484e9b392c29e35277725f6bc7aacabc729c
Forrest <forrest@smibs.com>
2009-03-13 12:18:15 -0600

    Compatability upgrades

--- javascripts/lines.js
+++ javascripts/lines.js
@@ -17,24 +17,25 @@ function initialize_line_drawing()
   if($("preload"))
     return;
   var preload_div = new Element("div",{"id":"preload","style":"visibility:hidden; height:0px; width:0px;"});
-  preload_div.insert(new Element("img",{"src":LINE_PATH+"/1up.gif"}));
-  preload_div.insert(new Element("img",{"src":LINE_PATH+"/1down.gif"}));
-  preload_div.insert(new Element("img",{"src":LINE_PATH+"/2up.gif"}));
-  preload_div.insert(new Element("img",{"src":LINE_PATH+"/2down.gif"}));
-  preload_div.insert(new Element("img",{"src":LINE_PATH+"/4up.gif"}));
-  preload_div.insert(new Element("img",{"src":LINE_PATH+"/4down.gif"}));
-  preload_div.insert(new Element("img",{"src":LINE_PATH+"/8up.gif"}));
-  preload_div.insert(new Element("img",{"src":LINE_PATH+"/8down.gif"}));
-  preload_div.insert(new Element("img",{"src":LINE_PATH+"/16up.gif"}));
-  preload_div.insert(new Element("img",{"src":LINE_PATH+"/16down.gif"}));
-  preload_div.insert(new Element("img",{"src":LINE_PATH+"/32up.gif"}));
-  preload_div.insert(new Element("img",{"src":LINE_PATH+"/32down.gif"}));
-  preload_div.insert(new Element("img",{"src":LINE_PATH+"/64up.gif"}));
-  preload_div.insert(new Element("img",{"src":LINE_PATH+"/64down.gif"}));
-  preload_div.insert(new Element("img",{"src":LINE_PATH+"/128up.gif"}));
-  preload_div.insert(new Element("img",{"src":LINE_PATH+"/128down.gif"}));
-  document.body.insert(preload_div);
-  document.body.insert(new Element("style").update(".line {position:absolute;}"))
+  preload_div.update( "<img src='"+LINE_PATH+"/1up.gif' />"+
+                      "<img src='"+LINE_PATH+"/1down.gif' />"+
+                      "<img src='"+LINE_PATH+"/2up.gif' />"+
+                      "<img src='"+LINE_PATH+"/2down.gif' />"+
+                      "<img src='"+LINE_PATH+"/4up.gif' />"+
+                      "<img src='"+LINE_PATH+"/4down.gif' />"+
+                      "<img src='"+LINE_PATH+"/8up.gif' />"+
+                      "<img src='"+LINE_PATH+"/8down.gif' />"+
+                      "<img src='"+LINE_PATH+"/16up.gif' />"+
+                      "<img src='"+LINE_PATH+"/16down.gif' />"+
+                      "<img src='"+LINE_PATH+"/32up.gif' />"+
+                      "<img src='"+LINE_PATH+"/32down.gif' />"+
+                      "<img src='"+LINE_PATH+"/64up.gif' />"+
+                      "<img src='"+LINE_PATH+"/64down.gif' />"+
+                      "<img src='"+LINE_PATH+"/128up.gif' />"+
+                      "<img src='"+LINE_PATH+"/128down.gif' />"+
+                      "<img src='"+LINE_PATH+"/256up.gif' />"+
+                      "<img src='"+LINE_PATH+"/256down.gif' />");
+  Element.extend(document.body).insert(preload_div);
   preloadedLineImages = preload_div.select('img');
 } 
 
@@ -42,9 +43,14 @@ var Line = Class.create({
   initialize: function(x1,y1,x2,y2)
   {
     initialize_line_drawing(); //incase called from within a script tag and not yet initialized
-    this.handle = new Element("img",{"class":"line"});
+    this.handle = new Element("img");
+    this.handle.addClassName("line")
     document.body.insert(this.handle);
-		this.update(x1,y1,x2,y2);
+		if(!this.update(x1,y1,x2,y2))
+		{
+		  this.destroy();
+		  return nil;
+		}
   },
   
   update: function(Ax,Ay,Bx,By)
@@ -63,7 +69,7 @@ var Line = Class.create({
   		lineIndex+=2;
 
     if(lineIndex<0 || lineIndex>preloadedLineImages.length-1)
-      return;
+      return false;
 
   	this.handle.src = preloadedLineImages[lineIndex].src;
   	with( this.handle.style )
@@ -78,6 +84,7 @@ var Line = Class.create({
   	this.x2 = Bx;
   	this.y1 = Ay;
   	this.y2 = By;
+  	return true;
   },
   
   destroy: function()
